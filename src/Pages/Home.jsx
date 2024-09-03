72918b2496cfafaeaa0a4d7876cde5c7d336f6f6
Jilson Joy <jilsonjoy98@gmail.com>
2024-09-03 15:13:41 +0530

    technologies completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,50 @@
 import React, { useState } from "react";
 import "../assets/css/commonStyle.css";
 import "../assets/css/mediaQuery.css";
+import Button from 'react-bootstrap/Button';
+import { ArrowUpRight } from 'lucide-react';
+import { Link } from "react-router-dom";
 
 function Home() {
-  // const [isPaused, setIsPaused] = useState(false);
 
-  // const handleMouseEnter = () => {
-  //   setIsPaused(true); // Pause the rotation when hovering over an item
-  // };
 
-  // const handleMouseLeave = () => {
-  //   setIsPaused(false); // Resume the rotation when mouse leaves the item
-  // };
+  const technologiesData = [
+    {
+      title: "React",
+      description: "A JavaScript library for building user interfaces.",
+      image: "https://i.pinimg.com/736x/70/22/fd/7022fde301338644bca180ebce7d51a7.jpg",
+      link: "https://reactjs.org/"
+    },
+    {
+      title: "Node.js",
+      description: "A JavaScript runtime built on Chrome's V8 JavaScript engine.",
+      image: "https://i.pinimg.com/736x/70/22/fd/7022fde301338644bca180ebce7d51a7.jpg",
+      link: "https://nodejs.org/"
+    },
+    {
+      title: "GraphQL",
+      description: "A query language for APIs and a server-side runtime for executing those queries.",
+      image: "https://i.pinimg.com/736x/70/22/fd/7022fde301338644bca180ebce7d51a7.jpg",
+      link: "https://graphql.org/"
+    },
+    {
+      title: "GraphQL",
+      description: "A query language for APIs and a server-side runtime for executing those queries.",
+      image: "https://i.pinimg.com/736x/70/22/fd/7022fde301338644bca180ebce7d51a7.jpg",
+      link: "https://graphql.org/"
+    }
+  ];
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <>
       <section className="hero-container">
@@ -30,20 +63,11 @@ function Home() {
               </p>
             </div>
             <div className="col-md-6">
-              {/* <div className="hero-image-content">
-              
-      <ul className={`list ${isPaused ? 'paused' : ''}`}>
-        <li className="item" style={{ '--i': 0 }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></li>
-        <li className="item" style={{ '--i': 1 }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></li>
-        <li className="item" style={{ '--i': 2 }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></li>
-        <li className="item" style={{ '--i': 3 }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></li>
-        <li className="item" style={{ '--i': 4 }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}></li>
-      </ul>
-    </div> */}
+
               <div className="wrapper">
                 <div className="img_container">
                   <input type="radio" name="slide" id="c1" checked />
-                  <label for="c1" class="card">
+                  <label for="c1" className="card">
                     <div className="card_row">
                       <div className="icon">1</div>
                       <div className="description">
@@ -75,7 +99,7 @@ function Home() {
                     </div>
                   </label>
                   <input type="radio" name="slide" id="c4" />
-                  <label for="c4" class="card">
+                  <label for="c4" className="card">
                     <div className="card_row">
                       <div className="icon">4</div>
                       <div className="description">
@@ -93,22 +117,49 @@ function Home() {
       </section>
 
 
-      {/*  */}
-      <section>
-        <div className="container">
-          <div className="row justify-content-center align-items-center">
-            <div className="col-md-6 text-center text-md-left">
-
-              <p className="hero-caption">
-                "Elevating user experience to its pinnacle."
-              </p>
-            </div>
-            <div className="col-md-6">
-
+      {/* Technologies   */}
+      <section >
+        <div className="row">
+          <div className="col-md d-flex justify-content-center align-items-center">
+            <h1>Technologies</h1>
+          </div>
+        </div>
+        <div className="container technologies mt-5">
+          <div className="row d-flex justify-content-between align-items-center ">
+            <div className="col-md">
+              <div className="row align-items-center">
+                {technologiesData.map((tech, index) => (
+                  <div className="col-md-6">
+                    <div key={index} className="row p-3">
+                      <div className="col-md-6">
+                        <h3>{tech.title}</h3>
+                        <p>{tech.description}</p>
+                        <Link to={tech.link} style={{ textDecoration: 'none' }}>
+                          <div className="button-with-text">
+                            <button className="readMore">
+                              <ArrowUpRight className="icon" />
+                              <span className="readMore-text">Read More</span>
+                            </button>
+                          </div>
+                        </Link>
+                      </div >
+                      <div className="col-md-6">
+                        <img
+                          src={tech.image}
+                          alt={tech.title}
+                          className="card_img"
+                          style={{ width: '100%', height: 'auto' }}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </section>
+
     </>
   );
 }
