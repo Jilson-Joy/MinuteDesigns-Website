13d116ec6bb5d931ef76472165ec17eb1dfe28f1
rosh222 <roshin.minute@gmail.com>
2024-09-13 15:55:21 +0530

    landing page changes

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,10 +3,9 @@ import React, { useState, useEffect, useRef } from "react";
 import "../assets/css/LandingPage.css";
 import "../assets/css/commonStyle.css";
 import "../assets/css/mediaQuery.css";
-
-
-
 function LandingPage() {
+
+
   return (
     <>
       <section className="hero-container">
@@ -53,68 +52,124 @@ function LandingPage() {
           </div>
           <div className="col-md-3 ">
         <div className="main-dot">
-  <ul >
-    <li className="high-ball"><span class="ball-1"></span></li>
-    <li className="high-ball"><span class="ball-2"></span></li>
-    <li className="high-ball"><span class="ball-3"></span></li>
-    <li className="high-ball"><span class="ball-4"></span></li>
-    <li className="high-ball"><span class="ball-5"></span></li>
-    <li className="high-ball"><span class="ball-6"></span></li>
-  </ul>
-</div>
-</div>
-</div>
+        <ul>
+                  <li className="high-ball" >
+                    <span className="ball-1"></span>
+                  </li>
+                  <li className="high-ball">
+                    <span className="ball-2"></span>
+                  </li>
+                  <li className="high-ball" >
+                    <span className="ball-3"></span>
+                  </li>
+                  <li className="high-ball">
+                    <span className="ball-4"></span>
+                  </li>
+                  <li className="high-ball" >
+                    <span className="ball-5"></span>
+                  </li>
+                  <li className="high-ball">
+                    <span className="ball-6"></span>
+                  </li>
+                </ul>
+                </div>
+                </div>
+                </div>
       </div>
     </section>
     
 
 
       {/* Services section */}
-      <section className="parallax-section">
-        <div className="container">
-          <div className="col-md-12 d-flex align-items-center mb-3">
-            <h5 className="head-text">Our Services</h5>
-          </div>
+ <section className="parallax-section">
+  <div className="container">
+    <div className="col-md-12 d-flex align-items-center mb-3">
+      <h5 className="head-text">Our Services</h5>
+    </div>
 
-          <div className="row d-flex justify-content-between align-items-center  mb-2">
-            {/* Web Application Section */}
-            <div className="col-md-4 service-head">
-              <div className="service-detail">
-                <h3>Web Application</h3>
-              </div>
-              <div className="service-desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                    ipsam velit eveniet natus commodi consequuntur quae, est
-                  </p>
-             </div>
-            </div>
-            <div className="col-md-4 service-head">
-              <div className="service-detail ">
-                <h3>Mobile Application</h3>
-              </div>
-              <div className="service-desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                    ipsam velit eveniet natus commodi consequuntur quae, est
-                  </p>
-             </div>
-            </div>
-            <div className="col-md-4 service-head">
-              <div className="service-detail ">
-                <h3>E-Commerce</h3>
-              </div>
+    <div className="row d-flex justify-content-between align-items-center  mb-2 ladder-layout">
 
-             <div className="service-desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                    ipsam velit eveniet natus commodi consequuntur quae, est
-                  </p>
-             </div>
-            </div>
-          </div>
+      {/* Web Application Section */}
+      <div className="col-md-4 service-head">
+        <div className="service-detail">
+          <h3>Web Application</h3>
         </div>
-      </section>
+        <div className="service-desc">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
+            velit eveniet natus commodi consequuntur quae, est.
+          </p>
+        </div>
+      </div>
+
+      {/* Mobile Application Section */}
+      <div className="col-md-4 service-head">
+        <div className="service-detail">
+          <h3>Mobile Application</h3>
+        </div>
+        <div className="service-desc">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
+            velit eveniet natus commodi consequuntur quae, est.
+          </p>
+        </div>
+      </div>
+
+      {/* E-Commerce Section */}
+      <div className="col-md-4 service-head">
+        <div className="service-detail">
+          <h3>E-Commerce</h3>
+        </div>
+        <div className="service-desc">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
+            velit eveniet natus commodi consequuntur quae, est.
+          </p>
+        </div>
+      </div>
+
+        {/* ui-ux Section */}
+        {/* <div className="col-md-4 service-head">
+        <div className="service-detail">
+        <h3>UI/UX</h3>
+        </div>
+        <div className="service-desc">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
+            velit eveniet natus commodi consequuntur quae, est.
+          </p>
+        </div>
+      </div> */}
+
+            {/* Architecture Section */}
+           {/* <div className="col-md-4 service-head">
+        <div className="service-detail">
+        <h3>Architecture</h3>
+        </div>
+        <div className="service-desc">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
+            velit eveniet natus commodi consequuntur quae, est.
+          </p>
+        </div>
+      </div> */}
+       {/* 3D Rendering Section */}
+        {/* <div className="col-md-4 service-head">
+        <div className="service-detail">
+            <span></span>
+        <h3> 3D Rendering Service </h3>
+        </div>
+        <div className="service-desc">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
+            velit eveniet natus commodi consequuntur quae, est.
+          </p>
+        </div>
+      </div> */}
+    </div>
+  </div>
+</section>
+
       {/* Add services or other content here */}
     </>
   );
