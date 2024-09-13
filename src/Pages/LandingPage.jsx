b77b3bb802698423eca6898b276529e7651680f0
Jilson Joy <jilsonjoy98@gmail.com>
2024-09-13 17:31:59 +0530

    added Technologies

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 import "../assets/css/LandingPage.css";
 import "../assets/css/commonStyle.css";
 import "../assets/css/mediaQuery.css";
+import Technologies from "../components/technologies/Technologies";
 
 
 
@@ -42,31 +43,31 @@ function LandingPage() {
           </div>
         </div>
       </section>
-          {/* Highlight text */}
-          <section>
-      <div className="container highlight-container  mt-5">
-      <div className="row d-flex justify-content-between align-items-center  mb-2">
-        <div className="col-md-9 service-head">
-          <p className="about-text">
-            <b >Transforming Ideas into Reality with <br /> Expert Web & App Solutions</b>
-          </p>
+      {/* Highlight text */}
+      <section>
+        <div className="container highlight-container  mt-5">
+          <div className="row d-flex justify-content-between align-items-center  mb-2">
+            <div className="col-md-9 service-head">
+              <p className="about-text">
+                <b >Transforming Ideas into Reality with <br /> Expert Web & App Solutions</b>
+              </p>
+            </div>
+            <div className="col-md-3 ">
+              <div className="main-dot">
+                <ul >
+                  <li className="high-ball"><span class="ball-1"></span></li>
+                  <li className="high-ball"><span class="ball-2"></span></li>
+                  <li className="high-ball"><span class="ball-3"></span></li>
+                  <li className="high-ball"><span class="ball-4"></span></li>
+                  <li className="high-ball"><span class="ball-5"></span></li>
+                  <li className="high-ball"><span class="ball-6"></span></li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="col-md-3 ">
-        <div className="main-dot">
-  <ul >
-    <li className="high-ball"><span class="ball-1"></span></li>
-    <li className="high-ball"><span class="ball-2"></span></li>
-    <li className="high-ball"><span class="ball-3"></span></li>
-    <li className="high-ball"><span class="ball-4"></span></li>
-    <li className="high-ball"><span class="ball-5"></span></li>
-    <li className="high-ball"><span class="ball-6"></span></li>
-  </ul>
-</div>
-</div>
-</div>
-      </div>
-    </section>
-    
+        </div>
+      </section>
+
 
 
       {/* Services section */}
@@ -83,39 +84,44 @@ function LandingPage() {
                 <h3>Web Application</h3>
               </div>
               <div className="service-desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                    ipsam velit eveniet natus commodi consequuntur quae, est
-                  </p>
-             </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                  ipsam velit eveniet natus commodi consequuntur quae, est
+                </p>
+              </div>
             </div>
             <div className="col-md-4 service-head">
               <div className="service-detail ">
                 <h3>Mobile Application</h3>
               </div>
               <div className="service-desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                    ipsam velit eveniet natus commodi consequuntur quae, est
-                  </p>
-             </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                  ipsam velit eveniet natus commodi consequuntur quae, est
+                </p>
+              </div>
             </div>
             <div className="col-md-4 service-head">
               <div className="service-detail ">
                 <h3>E-Commerce</h3>
               </div>
 
-             <div className="service-desc">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                    ipsam velit eveniet natus commodi consequuntur quae, est
-                  </p>
-             </div>
+              <div className="service-desc">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                  ipsam velit eveniet natus commodi consequuntur quae, est
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </section>
       {/* Add services or other content here */}
+
+      {/* Technologies */}
+<section>
+  <Technologies/>
+</section>
     </>
   );
 }
