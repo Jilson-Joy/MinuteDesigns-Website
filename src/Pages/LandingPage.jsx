b1afae1dea1663d9da3d24b1095184ee117c50ec
meroshin <me2roshinramesh@gmail.com>
2024-09-17 16:24:15 +0530

    new landing

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,16 +1,134 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect} from "react";
 // import "../assets/css/Home.css";
 import "../assets/css/LandingPage.css";
 import "../assets/css/commonStyle.css";
 import "../assets/css/mediaQuery.css";
+import WebDev from "../assets/images/web-developer.gif";
 function LandingPage() {
 
+
+// services
+
+
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+      setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+          window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
 
   return (
     <>
       <section className="hero-container">
         <div className="container">
           <div className="row align-items-center">
+          <div className="col-md-6">
+          <div className="dot-container">
+
+          <div className="dot" style={{ backgroundColor: 'transparent' }}></div>
+  <div className="dot"></div>
+  <div className="dot"></div>
+  <div className="dot"></div>
+  <div className="dot"></div>
+  <div className="dot"></div>
+
+
+                <div className="group top">
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                </div>
+                <div className="group top">
+                 
+                </div>
+                <div className="group right">
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                </div>
+                <div className="group left">
+                <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    M
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    I
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    N
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    U
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    T
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    E
+                  </span>
+                </div>
+                <div className="middle">
+                  <span ></span>
+                </div>
+                <div className="group right">
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>D</span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>E</span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>S</span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>I</span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>G</span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>N</span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>S</span>
+                  <span>•</span>
+                  <span>•</span>
+                </div>
+                <div className="group left">
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                </div>
+                <div className="group bottom">
+                
+                </div>
+                <div className="group bottom">
+                  <span></span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                  <span>•</span>
+                </div>
+              </div>
+             
+            </div>
+
+            
+     
             <div className="col-md-6">
               <h1 className="hero-text">
                 Design
@@ -23,11 +141,7 @@ function LandingPage() {
                 "Elevating user experience to its pinnacle."
               </p>
             </div>
-            <div className="col-md-6">
-              {/* <div>
-                <h2>M</h2>
-              </div> */}
-              <div class="cont">
+              {/* <div class="cont">
                 <div class="dots d1"></div>
                 <div class="dots d2"></div>
                 <div class="dots d3"></div>
@@ -36,13 +150,29 @@ function LandingPage() {
                 <div class="dots d6"></div>
                 <div class="dots d7"></div>
                 <div class="dots d8"></div>
-              </div>
-            </div>
+              </div> */}
+   
           </div>
         </div>
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+     
       </section>
           {/* Highlight text */}
-          <section>
+          {/* <section>
       <div className="container highlight-container  mt-5">
       <div className="row d-flex justify-content-between align-items-center  mb-2">
         <div className="col-md-9 service-head">
@@ -76,7 +206,52 @@ function LandingPage() {
                 </div>
                 </div>
       </div>
-    </section>
+    </section> */}
+
+
+
+
+
+
+
+<section>
+<div className="container">
+<div className="col-md-12 d-flex align-items-center mb-3">
+      <h5 className="head-text">Our Services</h5>
+    </div>
+    <div className="row d-flex justify-content-between align-items-center mb-2">
+
+          <div className="col-md-3 text-center service-item">
+            <div className="service-icon">
+              <img src={WebDev} alt="Web Development" width={150} />
+            </div>
+            <div className="service-title">
+              <h3>Web Application</h3>
+            </div>
+            <div className="service-description">
+              <p>We create responsive and robust web applications tailored to your business needs.</p>
+            </div>
+          </div>
+      
+
+
+
+
+
+    </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
     
 
 
