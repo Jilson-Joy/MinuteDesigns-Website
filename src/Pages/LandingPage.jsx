1f71e53be7243ad09c396b85a390044a2c9dc4f6
meroshin <me2roshinramesh@gmail.com>
2024-09-17 16:28:13 +0530

    landing page chnages

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,26 +1,23 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 // import "../assets/css/Home.css";
 import "../assets/css/LandingPage.css";
 import "../assets/css/commonStyle.css";
 import "../assets/css/mediaQuery.css";
 import WebDev from "../assets/images/web-developer.gif";
 function LandingPage() {
-
-
-// services
-
+  // services
 
   const [scrollY, setScrollY] = useState(0);
 
   const handleScroll = () => {
-      setScrollY(window.scrollY);
+    setScrollY(window.scrollY);
   };
 
   useEffect(() => {
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-          window.removeEventListener('scroll', handleScroll);
-      };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
@@ -28,16 +25,17 @@ function LandingPage() {
       <section className="hero-container">
         <div className="container">
           <div className="row align-items-center">
-          <div className="col-md-6">
-          <div className="dot-container">
-
-          <div className="dot" style={{ backgroundColor: 'transparent' }}></div>
-  <div className="dot"></div>
-  <div className="dot"></div>
-  <div className="dot"></div>
-  <div className="dot"></div>
-  <div className="dot"></div>
-
+            <div className="col-md-6">
+              <div className="dot-container">
+                <div
+                  className="dot"
+                  style={{ backgroundColor: "transparent" }}
+                ></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
+                <div className="dot"></div>
 
                 <div className="group top">
                   <span>•</span>
@@ -46,15 +44,14 @@ function LandingPage() {
                   <span>•</span>
                   <span>•</span>
                   <span>•</span>
+         
                 </div>
                 <div className="group top">
-                 
+                  
                 </div>
                 <div className="group right">
                   <span>•</span>
-                  <span>•</span>
-                  <span>•</span>
-                  <span>•</span>
+             
                   <span>•</span>
                   <span>•</span>
                   <span>•</span>
@@ -63,7 +60,8 @@ function LandingPage() {
                   <span>•</span>
                 </div>
                 <div className="group left">
-                <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                  
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
                     M
                   </span>
                   <span style={{ color: "#ee964b", fontWeight: "bold" }}>
@@ -81,20 +79,35 @@ function LandingPage() {
                   <span style={{ color: "#ee964b", fontWeight: "bold" }}>
                     E
                   </span>
+                  <span>•</span>
+                  <span>•</span>
                 </div>
                 <div className="middle">
-                  <span ></span>
+                  <span></span>
                 </div>
                 <div className="group right">
-                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>D</span>
-                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>E</span>
-                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>S</span>
-                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>I</span>
-                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>G</span>
-                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>N</span>
-                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>S</span>
-                  <span>•</span>
-                  <span>•</span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    D
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    E
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    S
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    I
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    G
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    N
+                  </span>
+                  <span style={{ color: "#ee964b", fontWeight: "bold" }}>
+                    S
+                  </span>
+                
                 </div>
                 <div className="group left">
                   <span>•</span>
@@ -108,9 +121,7 @@ function LandingPage() {
                   <span>•</span>
                   <span>•</span>
                 </div>
-                <div className="group bottom">
-                
-                </div>
+                <div className="group bottom"></div>
                 <div className="group bottom">
                   <span></span>
                   <span>•</span>
@@ -121,14 +132,10 @@ function LandingPage() {
                   <span>•</span>
                   <span>•</span>
                   <span>•</span>
-                  <span>•</span>
                 </div>
               </div>
-             
             </div>
 
-            
-     
             <div className="col-md-6">
               <h1 className="hero-text">
                 Design
@@ -141,7 +148,7 @@ function LandingPage() {
                 "Elevating user experience to its pinnacle."
               </p>
             </div>
-              {/* <div class="cont">
+            {/* <div class="cont">
                 <div class="dots d1"></div>
                 <div class="dots d2"></div>
                 <div class="dots d3"></div>
@@ -151,28 +158,11 @@ function LandingPage() {
                 <div class="dots d7"></div>
                 <div class="dots d8"></div>
               </div> */}
-   
           </div>
         </div>
-
-
-     
-
-
-
-
-
-
-
-
-
-
-
-
-     
       </section>
-          {/* Highlight text */}
-          {/* <section>
+      {/* Highlight text */}
+      {/* <section>
       <div className="container highlight-container  mt-5">
       <div className="row d-flex justify-content-between align-items-center  mb-2">
         <div className="col-md-9 service-head">
@@ -208,103 +198,79 @@ function LandingPage() {
       </div>
     </section> */}
 
-
-
-
-
-
-
-<section>
-<div className="container">
-<div className="col-md-12 d-flex align-items-center mb-3">
-      <h5 className="head-text">Our Services</h5>
-    </div>
-    <div className="row d-flex justify-content-between align-items-center mb-2">
-
-          <div className="col-md-3 text-center service-item">
-            <div className="service-icon">
-              <img src={WebDev} alt="Web Development" width={150} />
-            </div>
-            <div className="service-title">
-              <h3>Web Application</h3>
-            </div>
-            <div className="service-description">
-              <p>We create responsive and robust web applications tailored to your business needs.</p>
+      <section>
+        <div className="container">
+          <div className="col-md-12 d-flex align-items-center mb-3">
+            <h5 className="head-text">Our Services</h5>
+          </div>
+          <div className="row d-flex justify-content-between align-items-center mb-2">
+            <div className="col-md-3 text-center service-item">
+              <div className="service-icon">
+                <img src={WebDev} alt="Web Development" width={150} />
+              </div>
+              <div className="service-title">
+                <h3>Web Application</h3>
+              </div>
+              <div className="service-description">
+                <p>
+                  We create responsive and robust web applications tailored to
+                  your business needs.
+                </p>
+              </div>
             </div>
           </div>
-      
-
-
-
-
-
-    </div>
-    </div>
-</section>
-
-
-
-
-
-
-
-
-
-
-
-    
-
+        </div>
+      </section>
 
       {/* Services section */}
- <section className="parallax-section">
-  <div className="container">
-    <div className="col-md-12 d-flex align-items-center mb-3">
-      <h5 className="head-text">Our Services</h5>
-    </div>
+      <section className="parallax-section">
+        <div className="container">
+          <div className="col-md-12 d-flex align-items-center mb-3">
+            <h5 className="head-text">Our Services</h5>
+          </div>
 
-    <div className="row d-flex justify-content-between align-items-center  mb-2 ladder-layout">
+          <div className="row d-flex justify-content-between align-items-center  mb-2 ladder-layout">
+            {/* Web Application Section */}
+            <div className="col-md-4 service-head">
+              <div className="service-detail">
+                <h3>Web Application</h3>
+              </div>
+              <div className="service-desc">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                  ipsam velit eveniet natus commodi consequuntur quae, est.
+                </p>
+              </div>
+            </div>
 
-      {/* Web Application Section */}
-      <div className="col-md-4 service-head">
-        <div className="service-detail">
-          <h3>Web Application</h3>
-        </div>
-        <div className="service-desc">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
-            velit eveniet natus commodi consequuntur quae, est.
-          </p>
-        </div>
-      </div>
+            {/* Mobile Application Section */}
+            <div className="col-md-4 service-head">
+              <div className="service-detail">
+                <h3>Mobile Application</h3>
+              </div>
+              <div className="service-desc">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                  ipsam velit eveniet natus commodi consequuntur quae, est.
+                </p>
+              </div>
+            </div>
 
-      {/* Mobile Application Section */}
-      <div className="col-md-4 service-head">
-        <div className="service-detail">
-          <h3>Mobile Application</h3>
-        </div>
-        <div className="service-desc">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
-            velit eveniet natus commodi consequuntur quae, est.
-          </p>
-        </div>
-      </div>
+            {/* E-Commerce Section */}
+            <div className="col-md-4 service-head">
+              <div className="service-detail">
+                <h3>E-Commerce</h3>
+              </div>
+              <div className="service-desc">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                  ipsam velit eveniet natus commodi consequuntur quae, est.
+                </p>
+              </div>
+            </div>
 
-      {/* E-Commerce Section */}
-      <div className="col-md-4 service-head">
-        <div className="service-detail">
-          <h3>E-Commerce</h3>
-        </div>
-        <div className="service-desc">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam
-            velit eveniet natus commodi consequuntur quae, est.
-          </p>
-        </div>
-      </div>
-
-        {/* ui-ux Section */}
-        {/* <div className="col-md-4 service-head">
+            {/* ui-ux Section */}
+            {/* <div className="col-md-4 service-head">
         <div className="service-detail">
         <h3>UI/UX</h3>
         </div>
@@ -317,7 +283,7 @@ function LandingPage() {
       </div> */}
 
             {/* Architecture Section */}
-           {/* <div className="col-md-4 service-head">
+            {/* <div className="col-md-4 service-head">
         <div className="service-detail">
         <h3>Architecture</h3>
         </div>
@@ -328,8 +294,8 @@ function LandingPage() {
           </p>
         </div>
       </div> */}
-       {/* 3D Rendering Section */}
-        {/* <div className="col-md-4 service-head">
+            {/* 3D Rendering Section */}
+            {/* <div className="col-md-4 service-head">
         <div className="service-detail">
             <span></span>
         <h3> 3D Rendering Service </h3>
@@ -341,9 +307,9 @@ function LandingPage() {
           </p>
         </div>
       </div> */}
-    </div>
-  </div>
-</section>
+          </div>
+        </div>
+      </section>
 
       {/* Add services or other content here */}
     </>
